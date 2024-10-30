1023b5889313a8dcdff7feb510f8037c404b6f03
Jose F. Romaniello <jose@okta.com>
2024-10-30 13:10:02 -0300

    Refactor FGARetriever

--- sdk/fga/langchain/rag.ts
+++ sdk/fga/langchain/rag.ts
@@ -1,71 +1,52 @@
 import { Document, DocumentInterface } from "@langchain/core/documents";
 import { BaseRetriever, BaseRetrieverInput } from "@langchain/core/retrievers";
-import { ClientCheckRequest, ConsistencyPreference, OpenFgaClient } from "@openfga/sdk";
+import { ClientCheckRequest as FGACheckRequest, ConsistencyPreference, OpenFgaClient } from "@openfga/sdk";
 
 import type { CallbackManagerForRetrieverRun } from "@langchain/core/callbacks/manager";
-type FGARetrieverArgsWithoutCheckFromDocument<T extends CheckRequest> = {
-  user: string;
+
+type CheckDocument = (
+  doc: DocumentInterface,
+  query: string
+) => FGACheckRequest;
+
+type FGARetrieverArgs = {
+
+  /**
+   * The retriever to wrap.
+   */
   retriever: BaseRetriever;
-  checkFromDocument: (
-    user: string,
-    doc: DocumentInterface<Record<string, any>>,
-    query: string
-  ) => T;
+
+  /**
+   * The FGA client to use for checking permissions.
+   */
+  fgaClient: OpenFgaClient;
+
   fields?: BaseRetrieverInput;
+
+  /**
+   * Optional function to create the check permission tuple.
+   */
+  buildQuery: CheckDocument;
 };
 
-type FGARetrieverArgs<T extends CheckRequest> =
-  FGARetrieverArgsWithoutCheckFromDocument<T> & {
-    accessByDocument: (checks: T[]) => Promise<Map<string, boolean>>;
-  };
-type CheckRequest = {
-  object: string;
-};
-
-export class FGARetriever<T extends CheckRequest> extends BaseRetriever {
+export class FGARetriever extends BaseRetriever {
   lc_namespace = ["langchain", "retrievers"];
-  private retriever: BaseRetriever;
-  private checkFromDocument: (
-    user: string,
-    doc: DocumentInterface<Record<string, any>>,
-    query: string
-  ) => T;
-  private user: string;
-  private accessByDocument: (checks: T[]) => Promise<Map<string, boolean>>;
 
-  constructor({
-    user,
-    retriever,
-    checkFromDocument,
-    accessByDocument,
-    fields,
-  }: FGARetrieverArgs<T>) {
-    super(fields);
-    this.user = user;
-    this.checkFromDocument = checkFromDocument;
-    this.retriever = retriever;
-    this.accessByDocument = accessByDocument as (
-      checks: CheckRequest[]
-    ) => Promise<Map<string, boolean>>;
+  private retriever: BaseRetriever;
+  private checkDocument: CheckDocument;
+  private fgaClient: OpenFgaClient;
+
+  constructor(params: FGARetrieverArgs) {
+    super(params.fields);
+    this.fgaClient = params.fgaClient;
+    this.checkDocument = params.buildQuery;
+    this.retriever = params.retriever;
   }
 
   static adaptFGA(
-    fgaClient: OpenFgaClient,
-    args: FGARetrieverArgsWithoutCheckFromDocument<ClientCheckRequest>
-  ): FGARetriever<ClientCheckRequest> {
-    const accessByDocument = async function accessByDocument(
-      checks: ClientCheckRequest[]
-    ): Promise<Map<string, boolean>> {
-      const results = await fgaClient.batchCheck(checks, {
-        consistency: ConsistencyPreference.HigherConsistency
-      });
-      return results.responses.reduce((c: Map<string, boolean>, v) => {
-        c.set(v._request.object, v.allowed || false);
-        return c;
-      }, new Map<string, boolean>());
-    };
-
-    return new FGARetriever({ ...args, accessByDocument });
+    args: FGARetrieverArgs
+  ): FGARetriever {
+    return new FGARetriever({ ...args });
   }
 
   async _getRelevantDocuments(
@@ -79,24 +60,39 @@ export class FGARetriever<T extends CheckRequest> extends BaseRetriever {
 
     const out = documents.reduce(
       (out, doc) => {
-        const check = this.checkFromDocument(this.user, doc, query);
+        const check = this.checkDocument(doc, query);
         out.checks.push(check);
         out.documentToObject.set(doc, check.object);
         return out;
       },
       {
-        checks: [] as T[],
+        checks: [] as FGACheckRequest[],
         documentToObject: new Map<
-          DocumentInterface<Record<string, any>>,
+          DocumentInterface,
           string
         >(),
       }
     );
+
     const { checks, documentToObject } = out;
     const resultsByObject = await this.accessByDocument(checks);
 
     return documents.filter(
-      (d, i) => resultsByObject.get(documentToObject.get(d) || "") === true
+      (d) => resultsByObject.get(documentToObject.get(d)!) ?? false
     );
   }
+
+  private async accessByDocument(
+    checks: FGACheckRequest[]
+  ): Promise<Map<string, boolean>> {
+    const results = await this.fgaClient.batchCheck(checks, {
+      consistency: ConsistencyPreference.HigherConsistency
+    });
+    return results.responses.reduce((c: Map<string, boolean>, v) => {
+      c.set(v._request.object, v.allowed || false);
+      return c;
+    }, new Map<string, boolean>());
+  }
 }
+
+
