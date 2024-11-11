9a54e49a85c5505296f215609e007afc037e2f9e
Cristian Douce <cristian@okta.com>
2024-11-11 12:03:37 -0300

    feedback

--- next.config.mjs
+++ next.config.mjs
@@ -25,8 +25,8 @@ export default withSentryConfig(withMDX(nextConfig), {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: "auth0",
-  project: "market0",
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
